ec4f800c64e3e0f9123e01303a1b86784c79d471
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-04 19:36:01 +1100

    Added a property init
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

---
+++ src/main/java/net/mavity/slabs_build_additions/propertymanager/SlabPropertyInit.java
@@ -0,0 +1,9 @@
+package net.mavity.slabs_build_additions.propertymanager;
+
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class SlabPropertyInit {
+    public static final BlockBehaviour.Properties SLABTYPEBLOCK = BlockBehaviour.Properties.of()
+            .noOcclusion();
+}
