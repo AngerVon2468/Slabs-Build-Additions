04b951fb50cdb612faa52e5ef25bd72cdf55f5c5
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-11 12:00:34 +1100

    Updated breaking speed, fixed sound type
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/slabs_build_additions/propertyregister/PropertyInit.java
+++ src/main/java/net/mavity/slabs_build_additions/propertyregister/PropertyInit.java
@@ -1,5 +1,6 @@
 package net.mavity.slabs_build_additions.propertyregister;
 
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 
@@ -7,5 +8,6 @@ public class PropertyInit {
     public static final BlockBehaviour.Properties SBA_VSLAB = BlockBehaviour.Properties.of()
             .noOcclusion()
             .mapColor(MapColor.STONE)
-            .strength(2f, 2f);
+            .strength(0.5f, 2f)
+            .sound(SoundType.WOOD);
 }
